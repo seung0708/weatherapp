d80bfb46ef47a53ed4e2e69fe9e11a5d6e7f67c3
Seung Hyun Kim <seung.kim0708@gmail.com>
2024-06-10 11:07:58 -0700

    updated the apikey file to .env

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const form = document.querySelector('.top-banner form');
 
+console.log(API_KEY)
 form.addEventListener('submit', e => {
     e.preventDefault() 
     const inputVal = input.value;
-    
+
 })
